611925d7079c002ae0d6361960def902ecbd3b32
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-10-31 19:41:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    // const newdriver = {...driver};
+    // newdriver[key] = value;
+    // return newdriver;
+    return Object.assign({}, driver, {[key]: value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value;
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key){
+    let newdriver = Object.assign({}, driver);
+    delete newdriver[key];
+    return newdriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key];
+    return driver;
+}
